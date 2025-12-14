bc49263972a34752e4f375e0c998b5ebd60b3509
mutiacyn <mutia24ti@mahasiswa.pcr.ac.id>
2025-12-14 22:05:08 +0700

    baru

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\DashboardController;
-
+use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
     return redirect()->route('dashboard');
@@ -10,3 +10,6 @@ Route::get('/', function () {
 
 Route::get('/dashboard', [DashboardController::class, 'index'])
     ->name('dashboard');
+
+    Route::get('/user', [UserController::class, 'index'])
+    ->name('user.index');
